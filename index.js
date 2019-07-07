0458a8d534f5a45ac5513311a4c902b41fd9e624
Robert Chen <robertchen@live.com>
2019-07-06 22:43:22 -0700

    Uploaded template

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express"),
+ autoprefixer = require('express-autoprefixer'),
+ lessMiddleware = require('less-middleware');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("view engine", "ejs");
+
+const staticPath = __dirname + '/public';
+app.use(lessMiddleware(staticPath));
+app.use(autoprefixer({browsers: ["last 3 versions", "> 1%"], cascade: false}));
+app.use(express.static(staticPath));
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
