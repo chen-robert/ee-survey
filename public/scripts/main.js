2b8b21211636a4657b799949f2f1436f1e8566c8
Robert Chen <robertchen@live.com>
2019-07-23 22:50:29 +0800

    Added some fun chat messages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,49 @@
 window.onload = () => {
+  let playCount = 0;
+
   Array.from(document.getElementsByClassName("panel")).forEach(panel => {
-    panel.onclick = () => panel.children[0].submit();
+    let confirm = true;
+    panel.onclick = () => {
+      if(playCount === 0) {
+        return log("Wow, you didn't even listen to the samples and you could tell?");
+      }else if(playCount === 1){
+        return log("One more to go! You can do it!")
+      }
+
+      if(confirm) {
+        confirm = false;
+
+        log("...");
+
+        logAfter("Are you absolutely sure? What if the fate of the universe rested on your decision?", 3 * 1000);
+
+      }
+      
+      panel.children[0].submit();
+    }
   });
+
+  const played = [];
+  Array.from(document.getElementsByTagName("audio")).forEach(audio => {
+    audio.onplay = () => {
+      if(played.includes(audio)) return;
+
+      played.push(audio);
+      playCount++;
+    }
+  });
+}
+
+const sentMessages = [];
+const log = (msg, preventDupes=true) => {
+  if(preventDupes && sentMessages.includes(msg)) return;
+
+  sentMessages.push(msg);
+  document.getElementById("chat").insertAdjacentHTML("afterbegin", `
+    <div class="chat-message">${msg}</div>
+  `)
+}
+
+const logAfter = (msg, time, preventDupes=true) => {
+  setTimeout(() => log(msg, preventDupes), time);
 }
